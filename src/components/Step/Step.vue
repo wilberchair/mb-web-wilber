f01a669cb9fbbd71960d596f6954746bb6c4d1db
wilberchair <wilberchair@gmail.com>
2024-07-17 23:29:36 -0300

    create step component

---
+++ src/components/Step/Step.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="step">
+    <h3>Etapa <span>{{ current }}</span> de 5</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Step',
+  props: ['current']
+}
+</script>
+
+<style lang="scss">
+  @import '../../assets/style/variables.scss';
+  @import './style.scss';
+</style>
